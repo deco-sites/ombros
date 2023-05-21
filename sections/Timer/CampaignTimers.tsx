55d7cebbb8c7d05d5c6eaefe7732d8f09b02224f
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2023-05-21 10:35:21 -0300

    Atualiza campaignTimers

--- sections/Timer/CampaignTimers.tsx
+++ sections/Timer/CampaignTimers.tsx
@@ -5,13 +5,13 @@ export interface Props {
   /**
    * @format date-time
    */
-  date: string;
-  text: string;
-  labelButton: string;
-  link: string;
+  date?: string;
+  text?: string;
+  labelButton?: string;
+  link?: string;
 }
 
-function CampaignTimer({ text, labelButton, date, link }: Props) {
+function CampaignTimer({ text = 'Texto de chamada', labelButton = 'Botão', date = 'Sun May 21 2023 10:27:31 GMT-0300', link = '#' }: Props) {
   return (
     <div class="bg-primary py-4">
       <div class="flex flex-wrap justify-center items-center lg:gap-16">
